9173891511a3a183251df053fb53aafd35b25d28
Chirs Greene <chigreenwork@gmail.com>
2023-08-12 09:33:19 -0400

    set up basic html, adds some stying to the quiz section, creates varriables for the js

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+var startButton = document.querySelector(".start-button");
+var timerDisplay = document.querySelector(".timer");
+var questionDisplay = document.querySelector(".questions");
+var questionDisplay = document.querySelector(".answer");
+
+var timer = 60
+var questionIndex = 0;
+var question = [
+    {questions: "What does HTML stand for?", 
+    answer: ["Hyper Text Markup Language", "Hyper Text Modality Language", "Human Text Made Language", "Human Text Modality Language"], 
+    correct: 1 },
+    {question: "What does CSS stand for?", 
+    answer: ["Computer System Standard", "Cascading Style Sheet", "Creative Styling Sheet", "Cascading System Standard"], 
+    correct: 1}
+]
+
+
+]
