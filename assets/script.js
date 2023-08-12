af63a01fefe845069ef978b893b615b4701647c8
Chirs Greene <chigreenwork@gmail.com>
2023-08-12 09:48:46 -0400

    start game button starts timer

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,11 @@
-var startButton = document.querySelector(".start-button");
-var timerDisplay = document.querySelector(".timer");
-var questionDisplay = document.querySelector(".questions");
-var questionDisplay = document.querySelector(".answer");
+var startButton = document.querySelector("#start-button");
+var timerDisplay = document.querySelector("#timer");
+var questionDisplay = document.querySelector("#question");
+var questionDisplay = document.querySelector("#answer");
 
 var timer = 60
 var questionIndex = 0;
-var question = [
+var questions = [
     {questions: "What does HTML stand for?", 
     answer: ["Hyper Text Markup Language", "Hyper Text Modality Language", "Human Text Made Language", "Human Text Modality Language"], 
     correct: 1 },
@@ -14,5 +14,24 @@ var question = [
     correct: 1}
 ]
 
+// Event listener for the start button
+startButton.addEventListener('click', function() {
+    startGame();
+})
 
-]
+// Function to start the game
+function startGame() {
+    questionIndex = 0;
+    // when is question index increase
+    timer = 60;
+    // displayQuestion();
+    // Start the timer
+    var interval = setInterval(function() {
+      timer--;
+      timerDisplay.textContent = "Time Left: " + timer;
+      if (timer <= 0 || questionIndex >= questions.length) {
+        clearInterval(interval);
+        // endGame();
+      }
+    }, 1000);
+  }
